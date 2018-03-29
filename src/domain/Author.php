b61be67cade83f42b01bd124352c6a733c5d49c0
Dick Brouwers <dick@insided.nl>
2018-03-29 15:23:05 +0200

    green, it raises an exception when an author has no name

--- src/domain/Author.php
+++ src/domain/Author.php
@@ -6,6 +6,15 @@ namespace InSided\DDD\domain;
 
 final class Author
 {
+    private $name;
 
+    public function __construct(string $name)
+    {
+        if (empty($name)) {
+            throw new \InvalidArgumentException('An author should have a name');
+        }
+
+        $this->name = $name;
+    }
 }
 
