875f6973175ccbf7d20de94b2ea05856235fbb3e
Dick Brouwers <dick@insided.nl>
2018-03-29 14:30:11 +0200

    content test is green by moving the str_len statements to the vo

--- src/domain/Content.php
+++ src/domain/Content.php
@@ -6,4 +6,23 @@ namespace InSided\DDD\domain;
 
 final class Content
 {
+    private $content;
+
+    public function __construct(string $content)
+    {
+        if (mb_strlen($content) < 3) {
+            throw new \InvalidArgumentException('the content is too short');
+        }
+
+        if (mb_strlen($content) > 80) {
+            throw new \InvalidArgumentException('the content is too long');
+        }
+
+        $this->content = $content;
+    }
+
+    public function __toString(): string
+    {
+        return $this->content;
+    }
 }
