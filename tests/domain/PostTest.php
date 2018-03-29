f2bcf460f7c4eedf658eb225f1441c1abab52f7a
Dick Brouwers <dick@insided.nl>
2018-03-29 14:33:27 +0200

    all green again

--- tests/domain/PostTest.php
+++ tests/domain/PostTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace unit\InSided\DDD\domain;
 
+use InSided\DDD\domain\Content;
 use InSided\DDD\domain\Post;
 use PHPUnit\Framework\TestCase;
 
@@ -11,7 +12,7 @@ final class PostTest extends TestCase
 {
     public function testItCanBeWrittenByAnAuthor()
     {
-        $content = 'This is the content of a post';
+        $content = new Content('This is the content of a post');
         $author = 'John Doe';
         $post = Post::write($content, $author);
 
@@ -23,7 +24,7 @@ final class PostTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
 
-        Post::write('12', 'John Doe');
+        Post::write(new Content('12'), 'John Doe');
     }
 
     public function testItRaisesAnErrorWhenTheContentIsLongerThenEightyCharacters()
@@ -36,6 +37,6 @@ final class PostTest extends TestCase
 
         $this->expectException(\InvalidArgumentException::class);
 
-        Post::write($contentThatIsTooLong, 'John Doe');
+        Post::write(new Content($contentThatIsTooLong), 'John Doe');
     }
 }
