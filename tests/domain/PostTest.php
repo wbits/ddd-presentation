92b8cd4877ed3674b56ebba54f78b96fb502f2ac
Dick Brouwers <dick@insided.nl>
2018-03-29 15:08:27 +0200

    red, it gets written with an id

--- tests/domain/PostTest.php
+++ tests/domain/PostTest.php
@@ -6,6 +6,7 @@ namespace unit\InSided\DDD\domain;
 
 use InSided\DDD\domain\Content;
 use InSided\DDD\domain\Post;
+use InSided\DDD\domain\PostId;
 use PHPUnit\Framework\TestCase;
 
 final class PostTest extends TestCase
@@ -18,5 +19,15 @@ final class PostTest extends TestCase
 
         self::assertEquals($content, $post->content());
         self::assertEquals($author, $post->author());
+    }
+
+    public function testItGetsWrittenWithAPostId()
+    {
+        $postId = new PostId('1');
+        $content = new Content('This is the content of a post');
+        $author = 'John Doe';
+        $post = Post::write($postId, $content, $author);
+
+        self::assertEquals($postId, $post->id());
     }
 }
