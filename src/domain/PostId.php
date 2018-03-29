45aa7076a9b2cbdf788d2ef808bde23303364b62
Dick Brouwers <dick@insided.nl>
2018-03-29 15:04:39 +0200

    green, it raises an exception when instantiated with an emtpy string

--- src/domain/PostId.php
+++ src/domain/PostId.php
@@ -6,6 +6,15 @@ namespace InSided\DDD\domain;
 
 final class PostId
 {
+    private $postId;
 
+    public function __construct(string $postId)
+    {
+        if (empty($postId)) {
+            throw new \InvalidArgumentException('a postId can not be empty');
+        }
+
+        $this->postId = $postId;
+    }
 }
 
